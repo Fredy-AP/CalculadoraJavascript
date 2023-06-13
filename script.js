1d167b6a208df4c46d94c77ceed6c772d5f21a38
FredyAP <103303538+Fredy-AP@users.noreply.github.com>
2023-06-12 20:06:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ function calcular(){
     const resultado=eval(valorPantalla)
     document.getElementById('pantalla').value=resultado
 }
+// Obtener el elemento de entrada de la calculadora
+var input = document.getElementById('pantalla');
+
+// Agregar un controlador de eventos al elemento de entrada
+input.addEventListener("keydown", function(event) {
+  // Verificar si se presionó la tecla "Enter"
+  if (event.key === "Enter") {
+    // Obtener el valor del input
+    var inputValue = input.value;
+
+    // Evaluar la expresión matemática
+    var result = eval(inputValue);
+
+    // Mostrar el resultado en la calculadora
+    input.value = result;
+
+    // Evitar que el formulario se envíe o la página se actualice
+    event.preventDefault();
+  }
+});
